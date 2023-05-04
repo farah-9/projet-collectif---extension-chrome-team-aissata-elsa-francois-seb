6f5584333f2c7b81c46de12be2c617e5c671d393
SebFoodJ <sebastien.fougere@gmail.com>
2023-05-04 10:13:29 +0200

    Icons et popUp
    Encore des pb d'affichage.

--- Page/page.js
+++ Page/page.js
@@ -3,11 +3,6 @@ const form = document.getElementById("options-form");
 const submitBtn = document.getElementById("submit-btn");
 const cancelBtn = document.getElementById("cancel-btn");
 
-// Affiche la pop-up lorsque la page est chargée
-window.onload = function() {
-  popup.style.display = "block";
-}
-
 // Masque la pop-up lorsque l'utilisateur clique sur le bouton "Valider"
 form.addEventListener("submit", (e) => {
   e.preventDefault();
