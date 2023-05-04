08df70cc85781b05bac4a1b02fe5e7bfe06fce9c
Elsa Catoire <elsa.catoire@gmail.com>
2023-05-04 10:43:41 +0200

    ajout fonctionnalité dys ok

---
+++ Script/content.js
@@ -0,0 +1,27 @@
+function dys() {
+    // Sélectionne tous les éléments de la page
+    let elements = document.querySelectorAll("*, h1, h2, h3, h4, h5, h6");
+    // Parcourt tous les éléments et applique un style
+    for (let i = 0; i < elements.length; i++) {
+        console.log(elements[i].tagName);
+        elements[i].style.fontFamily = "Arial";
+        elements[i].style.color = "#2E2E2E";
+        elements[i].style.backgroundColor = "#FFFFCC"
+        // Vérifie et modifie si la taille de police est inférieure à 14pt
+        if (parseFloat(getComputedStyle(elements[i]).fontSize) < 14) {
+            elements[i].style.fontSize = "14pt";
+        } else {
+            elements[i].style.fontSize = getComputedStyle(elements[i]).fontSize;
+        }
+        // Gère l'espacement des mots, lettres, lignes..
+        elements[i].style.lineHeight = "2";
+        elements[i].style.letterSpacing = "1pt";
+        elements[i].style.wordSpacing = "2pt";
+        elements[i].style.textAlign = "left";
+        // Met en surbrillance les liens cliquables en bleu
+        if (elements[i].tagName === "A" && elements[i].hasAttribute("href")) {
+            elements[i].style.color = "blue";
+        }
+    }
+}
+dys()
